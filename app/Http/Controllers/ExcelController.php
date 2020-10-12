9809d14813264ca940d0d8e094110228e6519c5b
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2020-10-12 09:30:28 +0200

    Update ExcelController.php

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
+use App\Models\Tree;
 use App\Exports\DatasExport;
+use App\Mail\DataSent;
 
 class ExcelController extends Controller
 {
@@ -12,14 +15,29 @@ class ExcelController extends Controller
     {
         $data = $request->all();
 
-        $export = new DatasExport([
+        $export = [
           ['Order number', $data['order']],
           ['Type', $data['type']],
           ['Size', $data['size']],
           ['Amount', $data['amount']],
           ['Label', $data['label']],
-        ]);
+        ];
 
-        return Excel::download($export, 'data.xlsx');
+        // $data = new Tree;
+        //
+        // $data->order = $request->order;
+        // $data->type = $request->type;
+        // $data->size = $request->size;
+        // $data->amount = $request->amount;
+        // $data->label = $request->label;
+        //
+        // $data->save();
+
+
+
+        Mail::to('t@d.dk')->send(new DataSent($export));
+
+        return redirect('/')->with('success', 'The order is sent');
     }
+
 }
