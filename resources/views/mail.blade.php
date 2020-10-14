1cbe10e2752591a51499182603a7f551dedc25be
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2020-10-14 16:11:14 +0200

    Update mail.blade.php

---
+++ resources/views/mail.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Email test</h1>
+    <p>See attachment</p>
+  </body>
+</html>
