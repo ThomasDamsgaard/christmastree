b44ea7b848e2bd2f4da947379bb9ad2e037e21a5
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2020-12-29 16:58:51 +0100

    Line item inputs are required

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -32,23 +32,23 @@
             <tr v-for="(input, index) in inputs">
               <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex items-center">
-                  <input v-model="input.slot" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" placeholder="xxx" name="slot[]">
+                  <input v-model="input.slot" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" placeholder="xxx" name="slot[]" required>
                 </div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex items-center">
-                  <input v-model="input.pallet" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" placeholder="xxx" name="pallet[]">
+                  <input v-model="input.pallet" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" placeholder="xxx" name="pallet[]" required>
                 </div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
-                <input v-model="input.type" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" list="type" placeholder="xxx" name="type[]">
+                <input v-model="input.type" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" list="type" placeholder="xxx" name="type[]" required>
                 <datalist id="type">
                   <option value="NGR">NGR</option>
                   <option value="NOB">NOB</option>
                 </datalist>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                <input v-model="input.label" class="flex-1 block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" list="label" placeholder="xxx" name="label[]">
+                <input v-model="input.label" class="flex-1 block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" list="label" placeholder="xxx" name="label[]" required>
                 <datalist id="label">
                   <option value="Green/White">Green/White</option>
                   <option value="Blue/White">Blue/White</option>
@@ -56,7 +56,7 @@
                 </datalist>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                <input v-model="input.size" class="flex-1 block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" list="size" placeholder="xxx" name="size[]">
+                <input v-model="input.size" class="flex-1 block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" list="size" placeholder="xxx" name="size[]" required>
                 <datalist id="size">
                   <option value="125-150">125-150</option>
                   <option value="150-175">150-175</option>
@@ -64,7 +64,7 @@
                 </datalist>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                <input v-model="input.amount" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" placeholder="xxx" name="amount[]">
+                <input v-model="input.amount" class="block w-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type="text" placeholder="xxx" name="amount[]" required>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                 <a @click="remove(index)" class="text-red-600 hover:text-red-900">Delete</a>
