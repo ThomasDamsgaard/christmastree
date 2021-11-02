1b5e9cd1e46626405dd682886384893d4867b5e5
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2021-11-02 14:14:54 +0100

    wip

--- app/Http/Controllers/DeliveryNoteController.php
+++ app/Http/Controllers/DeliveryNoteController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\DataSent;
+use PDF;
 use App\Exports\DatasExport;
 use App\Exports\TreesExport;
-use App\Http\Requests\StoreDeliveryNoteRequest;
-use App\Mail\DataSent;
 use App\Models\DeliveryNote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
+use App\Http\Requests\StoreDeliveryNoteRequest;
 
 class DeliveryNoteController extends Controller
 {
@@ -19,28 +20,42 @@ class DeliveryNoteController extends Controller
      * @param  \App\Http\Requests\StoreDeliveryNoteRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreDeliveryNoteRequest $request)
+    public function store(Request $request)
     {
 
-        $data = new DeliveryNote;
+        // dd($request);
 
-        $data->date = $request->date;
-        $data->order = $request->order;
-        $data->puller = $request->puller;
-        $data->trailer = $request->trailer;
-        $data->second_trailer = $request->second_trailer;
-        $data->supplier = $request->supplier;
-        $data->reference = $request->reference;
-        $data->phone = $request->phone;
-        $data->location = $request->location;
-        $data->slot = json_encode($request->slot);
-        $data->pallet = json_encode($request->pallet);
-        $data->type = json_encode($request->type);
-        $data->size = json_encode($request->size);
-        $data->amount = json_encode($request->amount);
-        $data->label = json_encode($request->label);
+        $deliveryNote = DeliveryNote::create([
+            'order' => 1,
+            'date' => \Carbon\Carbon::parse($request->date),
+            'departure' => \Carbon\Carbon::parse($request->departure),
+            'arrival' => \Carbon\Carbon::parse($request->arrival),
+            'puller' => $request->puller,
+            'trailer' => $request->trailer,
+            'second_trailer' => $request->second_trailer
+        ]);
 
-        $data->save();
+        $pdf = PDF::loadView('exports.pdf', $deliveryNote);
+
+        return $pdf->stream();
+
+        // $data->date = $request->date;
+        // $data->order = $request->order;
+        // $data->puller = $request->puller;
+        // $data->trailer = $request->trailer;
+        // $data->second_trailer = $request->second_trailer;
+        // $data->supplier = $request->supplier;
+        // $data->reference = $request->reference;
+        // $data->phone = $request->phone;
+        // $data->location = $request->location;
+        // $data->slot = json_encode($request->slot);
+        // $data->pallet = json_encode($request->pallet);
+        // $data->type = json_encode($request->type);
+        // $data->size = json_encode($request->size);
+        // $data->amount = json_encode($request->amount);
+        // $data->label = json_encode($request->label);
+
+        // $data->save();
 
         return redirect('/dashboard')->with('success', 'The order is sent');
     }
--- resources/views/exports/pdf.blade.php
+++ resources/views/exports/pdf.blade.php
@@ -1,17 +1,278 @@
 <!DOCTYPE html>
 <html style="margin: 0; padding: 0">
 <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <title>Cover</title>
+    {{-- <link href='https://fonts.googleapis.com/css?family=Roboto Mono' rel='stylesheet'> --}}
+
+    <style>
+        body {
+            background: #fff none;
+            font-family: DejaVu Sans, 'sans-serif';
+            font-size: 14px;
+        }
+
+        .container {
+            padding-top: 30px;
+        }
+        .invoice-head td {
+            padding: 0 8px;
+        }
+        .table th {
+            vertical-align: bottom;
+            font-weight: bold;
+            padding: 8px;
+            line-height: 14px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        .table tr.row td {
+            border-bottom: 1px solid #ddd;
+        }
+        .table td {
+            padding: 8px;
+            line-height: 14px;
+            text-align: left;
+            vertical-align: top;
+            border: 1px solid #ddd;
+        }
+        .flex {
+            display: flex;
+        }
+
+        .flex-col {
+            flex-direction: column;
+        }
+
+        .flex-row {
+            flex-direction: row;
+        }
+
+        .justify-center {
+            justify-items: center;
+        }
+
+        .justify-between {
+            justify-items: space-between;
+        }
+
+        .p-5 {
+            padding: 3rem !important;
+        }
+
+        .pt-5 {
+            padding-top: 3rem !important;
+        }
+
+        .px-5 {
+            padding-left: 3rem !important;
+            padding-right: 3rem !important;
+        }
+
+        .mt-5 {
+            margin-top: 3rem !important;
+        }
+
+        .mt-1 {
+            margin-top: 0.25rem !important;
+        }
+
+        .text-xl {
+            font-size: 1.25rem;
+        }
+
+        .text-lg {
+            font-size: 1rem;
+        }
+
+        .font-semibold {
+            font-weight: 600;
+        }
+
+        blockquote, dd, dl, figure, h1, h2, h3, h4, h5, h6, hr, p, pre {
+            margin: 0;
+        }
+        .antialiased {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+
+        .font-sans {
+            font-family: system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        }
+
+    </style>
 </head>
-<body>
-<div>
-    <h1> {{ $date }}</h1>
-    <p> {{ $order }}</p>
-    <p> {{ $departure }}</p>
-    <p> {{ $arrival }}</p>
-    <p> {{ $puller }}</p>
-    <p> {{ $trailer }}</p>
-    <p> {{ $second_trailer }}</p>
-</div>
+<body class="font-sans antialiased">
+    <table class="pt-5 px-5" width="550" style="margin-left: auto; margin-right: auto;">
+    <tr>
+        <td align="left">
+            <div>
+                <h2 class="font-semibold text-xl">Silva Green ApS</h2>
+            </div>
+        </td>
+        <td align="left">
+            <table width="100%">
+                <tr>
+                    <td align="left">
+                        <h2 class="font-semibold text-xl">Delivery Note</h2>
+                    </td>
+                    <td align="left">
+                        <p class="font-semibold text-lg">1</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <div>
+                <p>Rugårdsvej 784, DK-5462 Morud</p>
+                <p>Tel: +4564801133</p>
+                <p>Fax: +4564801177</p>
+                <p>Mobile: +4530517002</p>
+                <p>Mobile: +4540737078</p>
+                <p>www.silva-green.dk</p>
+            </div>
+        </td>
+        <td>
+            <div>
+                <p class="font-medium text-lg">Destination:</p>
+                <p>ETS Georges Digonnet</p>
+                <p>Route de Montfaucon</p>
+                <p>Lieu-dit Charron</p>
+                <p>43190 Tence</p>
+            </div>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <div>
+                <p>Date: {{ $date->format('d F Y') }}</p>
+                <p class="mt-1">Puller: {{ $puller }}</p>
+                <p class="mt-1">Trailer: {{ $trailer }}</p>
+                @if ($second_trailer)
+                    <p class="mt-1">Second Trailer: {{ $second_trailer }}</p>
+                @endif
+            </div>
+        </td>
+        <td>
+            <p> Departure Date: {{ $departure->format('d F Y') }}</p>
+            <p class="mt-1"> Estimated Arrival Date: {{ $arrival->format('d F Y') }}</p>
+        </td>
+    </tr>
+
+  </table>
+
+  <table width="550" style="margin-left: auto; margin-right: auto;">
+      <tr>
+          <td>
+              <table class="table" width="100%" style="float: left; padding-left: 3rem; border-collapse: collapse;">
+                <thead>
+                  <tr class="row">
+                    <th>#</th>
+                    <th>Pallet</th>
+                    <th>Species</th>
+                    <th>Label</th>
+                    <th>Size</th>
+                    <th>Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 0; $i < 15; $i++)
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td width="30%"></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                          </tr>
+                    @endfor
+
+                </tbody>
+              </table>
+          </td>
+          <td>
+              <table class="table" width="100%" style="float: left; padding-right: 3rem; border-collapse: collapse;">
+                <thead>
+                  <tr class="row">
+                    <th>Species</th>
+                    <th>Size</th>
+                    <th>Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 0; $i < 15; $i++)
+                        <tr>
+                            <td width="45%"></td>
+                            <td></td>
+                            <td></td>
+                          </tr>
+                    @endfor
+
+                </tbody>
+              </table>
+          </td>
+      </tr>
+  </table>
+
+
+
+
+
+
+
+  <!-- Invoice Table -->
+                {{-- <table width="75%" class="table" border="0">
+                    <tr>
+                        <th align="left">Description</th>
+                        <th align="right">Date</th>
+
+
+                        <th align="right">Amount</th>
+                    </tr>
+
+                    <!-- Display The Invoice Items -->
+                    @foreach ($invoice->invoiceItems() as $item)
+                        <tr class="row">
+                            <td colspan="2">{{ $item->description }}</td>
+
+
+
+                            <td>{{ $item->total() }}</td>
+                        </tr>
+                    @endforeach
+                </table> --}}
+
+    {{-- <div class="flex flex-row justify-between p-5">
+        <div>
+            <h4 class="font-semibold text-xl mt-5">Silva Green ApS</h4>
+            <p>Rugårdsvej 784, DK-5462 Morud</p>
+            <p>Tel: +4564801133</p>
+            <p>Fax: +4564801177</p>
+            <p>Mobile: +4530517002</p>
+            <p>Mobile: +4540737078</p>
+            <p>www.silva-green.dk</p>
+        </div>
+        <div class="flex justify-between">
+            <h4 class="font-semibold text-xl">Delivery Note</h4>
+            <p class="font-semibold text-xl">1</p>
+        </div>
+    </div>
+
+    <div>
+        <h1> {{ $date }}</h1>
+        <p> {{ $order }}</p>
+        <p> {{ $departure }}</p>
+        <p> {{ $arrival }}</p>
+        <p> {{ $puller }}</p>
+        <p> {{ $trailer }}</p>
+        <p> {{ $second_trailer }}</p>
+    </div> --}}
 </body>
 </html>
